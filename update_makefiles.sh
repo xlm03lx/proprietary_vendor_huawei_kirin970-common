22b8be86275801c4765c8f6ad7577062845d8eaf
xlm03lx <xlm03lx@gmail.com>
2020-01-05 15:12:58 +0800

    update pie

--- update_makefiles.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-find "proprietary/" -type f | sed 's_^proprietary.*$_&:system/& \\_g' | sed 's_system/proprietary/_system/_g' | sed 's_proprietary/_vendor/huawei/kirin970-common/proprietary/_g'
